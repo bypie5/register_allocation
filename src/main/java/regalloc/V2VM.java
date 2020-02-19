f276ed2562f0f4b63f887d9acf5688c918be4acc
Brandon Yi <bypie966@gmail.com>
2020-02-19 15:38:00 -0800

    Configured project to be able to access classes in vapor-parser.jar

--- src/main/java/regalloc/V2VM.java
+++ src/main/java/regalloc/V2VM.java
@@ -1,6 +1,13 @@
 package regalloc;
 
+import cs132.vapor.parser.VaporParser;
+
 public class V2VM {
     public static void main(String[] args) {
+        try {
+            VaporParser.run(null, 1, 1, null, false, null, false);
+        } catch (Exception e) {
+
+        }
     }
 }
