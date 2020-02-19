9ec07831d64f4e39aeaf4a2adbbb4bd34eabaef7
Brandon Yi <bypie966@gmail.com>
2020-02-19 15:40:42 -0800

    Clean up

--- src/main/java/regalloc/V2VM.java
+++ src/main/java/regalloc/V2VM.java
@@ -1,13 +1,7 @@
 package regalloc;
 
-import cs132.vapor.parser.VaporParser;
-
 public class V2VM {
     public static void main(String[] args) {
-        try {
-            VaporParser.run(null, 1, 1, null, false, null, false);
-        } catch (Exception e) {
 
-        }
     }
 }
